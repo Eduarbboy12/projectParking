57117d191a730c491e3635a83d51d8b776db1ada
carlos.diaz <carlos.diaz@CARLOS-DIAZ.ceiba.local>
2018-02-15 10:14:38 -0500

    #5 - creacion front en angular

---
+++ ProjectParking/bin/static/js/app.js
@@ -0,0 +1,102 @@
+var app = angular.module('myApp', ['ngResource']);
+
+app.controller('UserController', ['$scope', '$resource',function($scope,$resource) {
+    
+    function fetchAllUser(){
+        $scope.users = $resource('http://localhost:8080/user'
+        ).query(function(data){return data;});
+    };
+    fetchAllUser();
+    
+    $scope.refresh = function(){
+    	fetchAllUser();
+    };
+    
+    $scope.create = function(){
+    	User = $resource(
+    		    "http://localhost:8080/create",
+    		    {},
+    		    {save: {method:'PUT',isArray:false}}
+    	);
+    	
+    	var user = {};
+		
+		user.Id = $scope.userForm.Id;
+		user.Nombre = $scope.userForm.Nombre;
+		user.Apellido = $scope.userForm.Apellido;
+		user.Documento = $scope.userForm.Documento;
+		user.Telefono = $scope.userForm.Telefono;
+		user.Direccion = $scope.userForm.Direccion;
+		user.Mail = $scope.userForm.Mail;
+		user.Clave = $scope.userForm.Clave;
+		user.confirmPassword = "";
+		
+		$scope.Message = User.save(user);
+				
+		$scope.userForm.Id = "";
+		$scope.userForm.Nombre="";
+		$scope.userForm.Apellido="";
+		$scope.userForm.Documento="";
+		$scope.userForm.Telefono="";
+		$scope.userForm.Direccion="";
+		$scope.userForm.Mail="";
+		$scope.userForm.Clave="";
+    };
+    
+    $scope.deleteRec = function(){
+    	User = $resource(
+    		    "http://localhost:8080/delete/:id",
+    		    {},
+    		    {save: {method:'DELETE', params: {id: '@id'}}}
+    	);
+    	
+			
+		User.delete({id: $scope.userForm.Id}).then(function successCallback(response) {
+			$scope.Message = response;
+		}, function errorCallback(response) {
+		    
+		});
+				
+		$scope.userForm.Id = "";
+		$scope.userForm.Nombre="";
+		$scope.userForm.Apellido="";
+		$scope.userForm.Documento="";
+		$scope.userForm.Telefono="";
+		$scope.userForm.Direccion="";
+		$scope.userForm.Mail="";
+		$scope.userForm.Clave="";
+    };
+    
+    
+    $scope.update = function(){
+    		
+    	User = $resource(
+    		    "http://localhost:8080/update/:id",
+    		    {},
+    		    {save: {method:'PUT', params: {id: '@id'}}}
+    	);
+    	
+		var user = {};
+		
+		user.Id = $scope.userForm.Id;
+		user.Nombre = $scope.userForm.Nombre;
+		user.Apellido = $scope.userForm.Apellido;
+		user.Documento = $scope.userForm.Documento;
+		user.Telefono = $scope.userForm.Telefono;
+		user.Direccion = $scope.userForm.Direccion;
+		user.Mail = $scope.userForm.Mail;
+		user.Clave = $scope.userForm.Clave;
+		user.confirmPassword = "";
+		
+		$scope.Message = User.save({id: $scope.userForm.Id}, user);
+				
+		$scope.userForm.Id = "";
+		$scope.userForm.Nombre="";
+		$scope.userForm.Apellido="";
+		$scope.userForm.Telefono="";
+		$scope.userForm.Direccion="";
+		$scope.userForm.Mail="";
+		$scope.userForm.Clave="";
+    };
+    
+}]);
